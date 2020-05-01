7bbcb61814fc256c78ae1e2fc66edb0550ab8f91
packerchane <62860961+packerchane@users.noreply.github.com>
2020-04-30 21:09:28 -0400

    updated the files

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {}
+  
+variable "aws_secret_key" {}
+
+variable "cidr_vpc" {}
+variable "cidr_subnet" {}
+
+variable "availability_zone" {}
+
+variable "instance_ami" {}
+
+variable "instance_type" {}
+
